7ffe5e013f7accf49192ef1f2e8b4156263537a0
Hayden McGeady <hjm141@uclive.ac.nz>
2022-06-18 02:50:29 +1200

    dragable placable torches

---
+++ Assets/Scripts/Inventory/Torch.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Torch : Item
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public override void UseItem(Vector3 targetPos)
+    {
+        Instantiate(gameObject, targetPos, Quaternion.identity);
+    }
+}
